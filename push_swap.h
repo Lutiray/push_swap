442cb55c4a057dcfc0d5a1e7b37f780b933ca944
Natalia Eremina <nateremi@c1r6s3.42prague.com>
2026-03-26 17:04:19 +0100

    Add function safe_atoi for checking input

--- push_swap.h
+++ push_swap.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+
 typedef struct	s_node
 {
 	int	value;
--- src/init_stack.c
+++ src/init_stack.c
@@ -1,12 +1,40 @@
-#include "../push_swap.h"
+#include "push_swap.h"
 
-void	add_back(t_node **stack, int data)
+int	safe_atoi(char *str, int *result)
 {
-	t_node	*current;
-	t_node	*next;
+	long	num;
+	int		sign;
+	int		i;
 
-	if (*stack )
-	current->value = data;
+	num = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] && (str[i] == ' ' || str[i] == '\t'
+			|| str[i] == '\n' || str[i] == '\r'
+			|| str[i] == '\v' || str[i] == '\f'))
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	if (!ft_isdigit(str[i]))
+		return (0);
+	while (ft_isdigit(str[i]))
+	{
+		num = (num * 10) + (str[i] - '0');
+		if (sign == 1 && num > INT_MAX)
+			return (0);
+		if (sign == -1 && -num < INT_MIN)
+			return (0);
+		i++;
+	}
+	if (str[i])
+		return (0);
+	*result = (int)(num * sign);
+	return (1);
 }
 
 t_node	*init_stack(int argc, char **argv)
@@ -14,18 +42,43 @@ t_node	*init_stack(int argc, char **argv)
 	int		i;
 	char	**tmp;
 	t_node	*stack_a;
+	t_node	*new_node;
+	int		data;
 	
-	stack_a = NULL:
+	stack_a = NULL;
+	data = 0;
+	i = 0;
 	if (argc == 2)
-		tmp = ft_split(argv[i], ' ');
-	else 
+	{
+		tmp = ft_split(argv[1], ' ');
+		if (!tmp)
+			return (NULL);
+	}
+	else
 	{
 		i = 1;
 		tmp = argv;
 	}
+	if (tmp == NULL)
+		return (NULL);
 	while (tmp[i])
 	{
-		add_back(&stack_a, ft_atoi(tmp[i]));
+		if (!safe_atoi(tmp[i], &data))
+		{
+			if (argc == 2)
+				ft_free(tmp);
+			ft_lstdelone(&stack_a);
+			return (NULL);
+		}
+		new_node = ft_lstnew(data);
+		if (new_node == NULL)
+		{
+			if (argc == 2)
+				ft_free(tmp);
+			ft_lstdelone(&stack_a);
+			return (NULL);
+		}
+		ft_lstadd_back(&stack_a, new_node);
 		i++;
 	}
 	if (argc == 2)
